e6e2be6d24287eccef917f0482ea8d96cd3f64f1
Michael Munyaneza and Wendell Britt <michael@>
2016-08-29 20:06:41 -0700

    create basic calculator project (only add fuctionality)

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2) {
+
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a Number:"));
+var number2 = parseInt(prompt("Enter a Number:"));
+var result = add(number1,number2)
+alert(result);
